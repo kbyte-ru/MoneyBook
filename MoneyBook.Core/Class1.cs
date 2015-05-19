02e4aa64c6ac1378339bb307e5f95894095b0e5e
Aleksey Nemiro <aleksey@kbyte.ru>
2015-05-19 17:00:54 +0300

    Наброски

---
+++ MoneyBook.Core/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Data.SqlServerCe;
+
+namespace MoneyBook.Core
+{
+
+  public class Class1
+  {
+    
+  }
+
+}
