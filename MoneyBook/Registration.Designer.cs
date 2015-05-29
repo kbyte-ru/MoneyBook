81568005cc6fe48c3b2a7a1f46169acff5cbd5bb
Roman <tundrov@gmail.com>
2015-05-29 17:18:03 +0300

    Начало регистрационной формы.

---
+++ MoneyBook/Registration.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace MoneyBook.WinApp
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registration));
+            this.AcceptButton = new System.Windows.Forms.Button();
+            this.CancellButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ShowPasswordCheckBox = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // AcceptButton
+            // 
+            this.AcceptButton.Image = global::MoneyBook.WinApp.Properties.Resources.accept_button;
+            this.AcceptButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AcceptButton.Location = new System.Drawing.Point(100, 274);
+            this.AcceptButton.Name = "AcceptButton";
+            this.AcceptButton.Size = new System.Drawing.Size(75, 24);
+            this.AcceptButton.TabIndex = 0;
+            this.AcceptButton.Text = "Apply";
+            this.AcceptButton.UseVisualStyleBackColor = true;
+            // 
+            // CancellButton
+            // 
+            this.CancellButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancellButton.Image = global::MoneyBook.WinApp.Properties.Resources.cancel;
+            this.CancellButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CancellButton.Location = new System.Drawing.Point(181, 274);
+            this.CancellButton.Name = "CancellButton";
+            this.CancellButton.Size = new System.Drawing.Size(75, 24);
+            this.CancellButton.TabIndex = 1;
+            this.CancellButton.Text = "Cancel";
+            this.CancellButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Имя:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль:";
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(77, 44);
+            this.PasswordTextBox.MaxLength = 24;
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(179, 20);
+            this.PasswordTextBox.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // ShowPasswordCheckBox
+            // 
+            this.ShowPasswordCheckBox.AutoSize = true;
+            this.ShowPasswordCheckBox.Location = new System.Drawing.Point(77, 69);
+            this.ShowPasswordCheckBox.Name = "ShowPasswordCheckBox";
+            this.ShowPasswordCheckBox.Size = new System.Drawing.Size(179, 17);
+            this.ShowPasswordCheckBox.TabIndex = 6;
+            this.ShowPasswordCheckBox.Text = "отображать пароль при вводе";
+            this.ShowPasswordCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 92);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(231, 176);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancellButton;
+            this.ClientSize = new System.Drawing.Size(272, 309);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ShowPasswordCheckBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CancellButton);
+            this.Controls.Add(this.AcceptButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Registration";
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AcceptButton;
+        private System.Windows.Forms.Button CancellButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox ShowPasswordCheckBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
